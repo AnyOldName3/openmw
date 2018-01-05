5513938b424521c42b96b214153574b7249d4ef5
AnyOldName3 <krizdjali+github@gmail.com>
2018-01-05 22:34:57 +0000

    Fix shader compile error that was entirely my fault.

--- files/shaders/objects_vertex.glsl
+++ files/shaders/objects_vertex.glsl
@@ -49,10 +49,10 @@ varying vec3 passViewPos;
 varying vec3 passNormal;
 
 #if SHADOWS
-	@foreach shadow_texture_unit_index @shadow_texture_unit_list
-		uniform int shadowTextureUnit@shadow_texture_unit_index;
-		varying vec4 shadowSpaceCoords@shadow_texture_unit_index;
-	@endforeach
+    @foreach shadow_texture_unit_index @shadow_texture_unit_list
+        uniform int shadowTextureUnit@shadow_texture_unit_index;
+        varying vec4 shadowSpaceCoords@shadow_texture_unit_index;
+    @endforeach
 
     uniform bool shadowMapMode;
 #if !PER_PIXEL_LIGHTING
@@ -113,9 +113,10 @@ void main(void)
         // We only care about the depth buffer so we skip the 'colouring in', which should be faster on many GPUs.
 #if !PER_PIXEL_LIGHTING
         passAlpha = getAlpha(gl_Color);
-#endif
+#else
         passColor = gl_Color;
-		return;
+#endif // !PPL
+        return;
     }
 #endif // SHADOWS
 
@@ -128,11 +129,11 @@ void main(void)
     passNormal = gl_Normal.xyz;
 
 #if SHADOWS
-	// This matrix has the opposite handedness to the others used here, so multiplication must have the vector to the left. Alternatively it could be transposed after construction, but that's extra work for the GPU just to make the code look a tiny bit cleaner.
-	mat4 eyePlaneMat;
-	@foreach shadow_texture_unit_index @shadow_texture_unit_list
-		eyePlaneMat = mat4(gl_EyePlaneS[shadowTextureUnit@shadow_texture_unit_index], gl_EyePlaneT[shadowTextureUnit@shadow_texture_unit_index], gl_EyePlaneR[shadowTextureUnit@shadow_texture_unit_index], gl_EyePlaneQ[shadowTextureUnit@shadow_texture_unit_index]);
-		shadowSpaceCoords@shadow_texture_unit_index = viewPos * eyePlaneMat;
-	@endforeach
+    // This matrix has the opposite handedness to the others used here, so multiplication must have the vector to the left. Alternatively it could be transposed after construction, but that's extra work for the GPU just to make the code look a tiny bit cleaner.
+    mat4 eyePlaneMat;
+    @foreach shadow_texture_unit_index @shadow_texture_unit_list
+        eyePlaneMat = mat4(gl_EyePlaneS[shadowTextureUnit@shadow_texture_unit_index], gl_EyePlaneT[shadowTextureUnit@shadow_texture_unit_index], gl_EyePlaneR[shadowTextureUnit@shadow_texture_unit_index], gl_EyePlaneQ[shadowTextureUnit@shadow_texture_unit_index]);
+        shadowSpaceCoords@shadow_texture_unit_index = viewPos * eyePlaneMat;
+    @endforeach
 #endif // SHADOWS
 }
